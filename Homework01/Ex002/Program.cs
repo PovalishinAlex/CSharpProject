ba1456135d6f74da4eb3a705f8939ae83b89b82f
Aleksey Povalishin <povalishinalex@icloud.com>
2023-01-07 13:33:46 +0300

    add Ex002

---
+++ Homework01/Ex002/Program.cs
@@ -0,0 +1,25 @@
+﻿int numA = 22;
+int numB = 3;
+int numC = 9;
+int max = 0;
+if (numA > numB)
+{
+    max = numA;
+}
+
+else
+{
+    max = numB;
+}
+if (numC > max)
+{
+    max = numC;
+}
+else
+{
+    max = max;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+//На написание данной программы понадобилось 22 минуты.
