572dd827275bf7c4762eb1edc0233c41ace32f0a
Aleksey Povalishin <povalishinalex@icloud.com>
2023-01-07 13:01:16 +0300

    add Ex001

---
+++ Homework01/Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿int numA = -9;
+int numB = -3;
+int max = 0;
+if (numA > numB)
+{
+    max = numA;
+}
+else
+{
+    max = numB;
+}
+Console.Write("max = ");
+Console.Write(max);
+// На написание данной программы понадобилось 15 минут.
